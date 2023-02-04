4df9bb5d3dd097b85259d4497d895040e37024ba
jonas.bechthold <crossengage@AirJonaechthold.fritz.box>
2023-02-04 12:50:21 +0100

    initial commit

--- blocks/make_docker_block.py
+++ blocks/make_docker_block.py
@@ -2,9 +2,9 @@ from prefect.infrastructure.docker import DockerContainer
 
 # alternative to creating DockerContainer block in the UI
 docker_block = DockerContainer(
-    image="discdiver/prefect:zoom",  # insert your image here
+    image="01091989/prefect:zoomcamp",  # insert your image here
     image_pull_policy="ALWAYS",
     auto_remove=True,
 )
 
-docker_block.save("zoom", overwrite=True)
+docker_block.save("zoomcamp", overwrite=True)
