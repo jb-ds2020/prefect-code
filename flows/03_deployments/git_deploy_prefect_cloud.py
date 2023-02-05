75987f0e3b7da51cc2100083c5fb5920c67a3b56
jonas.bechthold <crossengage@MacBook-Air-Jonas-Bechthold.local>
2023-02-05 14:12:49 +0100

    cloud adj

--- flows/03_deployments/git_deploy_prefect_cloud.py
+++ flows/03_deployments/git_deploy_prefect_cloud.py
@@ -1,14 +1,9 @@
 from prefect.deployments import Deployment
 from ETL_web_gcs_bq_homework import etl_parent_flow
 
-from prefect.filesystems import GitHub
-
 from prefect_github.repository import GitHubRepository
 
 storage = GitHubRepository.load("github-prefect-code")
-
-# storage = GitHub.load("github-prefect-code")
-# storage = GitHub.load("jb-prefect")
 
 deployment = Deployment.build_from_flow(
     flow=etl_parent_flow,
